83e2436357451afecd34e5c40cca785b342ba110
Ryotaro Karikomi <ryotaro.karikomi@gmail.com>
2023-10-27 17:18:20 +0900

    Change variable name

--- big_report/big_report.c
+++ big_report/big_report.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 
 int main(){
-  int  alphabet_number_low = 0;
-  int  alphabet_counter_low[26];
+  int  alphabet_low_number = 0;
+  int  alphabet_low_counter[26];
   char alphabet_low[26];
-  int  alphabet_number_upp = 0;
-  int  alphabet_counter_upp[26];
+  int  alphabet_upp_number = 0;
+  int  alphabet_upp_counter[26];
   char alphabet_upp[26];
   
   int blank = 0;
 
+  //アルファベットの識別子を設定
   for(int alp = 'a'; alp <= 'z'; alp++){
-    alphabet_low[alphabet_number_low] = alp;
-    alphabet_number_low++;
+    alphabet_low[alphabet_low_number] = alp;
+    alphabet_low_number++;
   }
   for(int alp = 'A'; alp <= 'Z'; alp++){
-    alphabet_upp[alphabet_number_upp] = alp;
-    alphabet_number_upp++;
+    alphabet_upp[alphabet_upp_number] = alp;
+    alphabet_upp_number++;
   }
   for(int i = 0; i < 26; i++){
     printf("%c", alphabet_low[i]);
     printf("%c", alphabet_upp[i]);
   } printf("\n");
 
+  //アルファベットを認識
   FILE *fp;
   char sentence[] = "sentence.txt";
   char chr;
@@ -32,15 +34,16 @@ int main(){
     putchar(chr);
     for(int i = 0; i < 26; i++){
       if(chr == ' ') {blank++; break;}
-      else if(chr == alphabet_low[i]){alphabet_counter_low[i] += 1; break;}
-      else if(chr == alphabet_upp[i]){alphabet_counter_upp[i] += 1; break;}
+      else if(chr == alphabet_low[i]){alphabet_low_counter[i] += 1; break;}
+      else if(chr == alphabet_upp[i]){alphabet_upp_counter[i] += 1; break;}
     }
   }
 
   fclose(fp);
 
+  //結果を表示
   for(int i = 0; i < 26; i++){
-    printf("%c:%5d| %c:%5d\n", alphabet_low[i], alphabet_counter_low[i], alphabet_upp[i], alphabet_counter_upp[i]);
+    printf("%c:%5d| %c:%5d\n", alphabet_low[i], alphabet_low_counter[i], alphabet_upp[i], alphabet_upp_counter[i]);
   } printf("blank:%5d\n", blank); 
   
   return 0;
