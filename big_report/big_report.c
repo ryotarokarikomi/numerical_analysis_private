37855b471483f85608e49480bf1a6549deb14457
Ryotaro Karikomi <ryotaro.karikomi@gmail.com>
2023-10-27 13:06:48 +0900

    Add alphabet-counter

--- big_report/big_report.c
+++ big_report/big_report.c
@@ -2,8 +2,10 @@
 
 int main(){
   int  alphabet_number_low = 0;
+  int  alphabet_counter_low[26];
   char alphabet_low[26];
   int  alphabet_number_upp = 0;
+  int  alphabet_counter_upp[26];
   char alphabet_upp[26];
 
   for(int alp = 'a'; alp <= 'z'; alp++){
@@ -26,10 +28,17 @@ int main(){
 
   while((chr = fgetc(fp)) != EOF){
     putchar(chr);
+    for(int i = 0; i < 26; i++){
+      if     (chr == alphabet_low[i]){alphabet_counter_low[i] += 1; break;}
+      else if(chr == alphabet_upp[i]){alphabet_counter_upp[i] += 1; break;}
+    }
   }
 
   fclose(fp);
 
-
+  for(int i = 0; i < 26; i++){
+    printf("%c:%5d\n", alphabet_low[i], alphabet_counter_low[i]);
+  } 
+  
   return 0;
 }
